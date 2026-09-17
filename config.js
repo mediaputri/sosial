419941d84f5e6ca2cf5f2c8aecc1d6c15974df6e
mediaputri <mediaputrii2026@gmail.com>
2026-09-17 14:14:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const API_URL =
-https://script.google.com/macros/s/AKfycby8R9C4fopjyFwJ7zY__JTbXX-Jjhm315v12LUKjhtr9w2QBybCkluwtuMJcQuGHIqO/exec";
+https://script.google.com/macros/s/AKfycbwQ-AG7f5K-TCSobT7hUz9o087YYZnioqSy5rjL5vKV7SWV_-O7QgvQjg3gtMjECGVO/exec";
 //"https://script.google.com/macros/s/AKfycbwQ-AG7f5K-TCSobT7hUz9o087YYZnioqSy5rjL5vKV7SWV_-O7QgvQjg3gtMjECGVO/exec";
+//https://script.google.com/macros/s/AKfycby8R9C4fopjyFwJ7zY__JTbXX-Jjhm315v12LUKjhtr9w2QBybCkluwtuMJcQuGHIqO/exec";
 
